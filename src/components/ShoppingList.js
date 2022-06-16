ecfcbc2490b834cbfb508e7db56ee6fd349c300d
Jaime Montes de Oca <jaime0422@live.com>
2022-06-16 15:03:59 -0700

    Completed Assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -18,6 +18,27 @@ function ShoppingList() {
         setItems([...items, newItem]);
     }
 
+
+    function handleUpdateItem(updatedItem) {
+        const updatedItems = items.map(item => {
+            if (item.id === updatedItem.id) {
+                return updatedItem;
+            } else {
+                return item;
+            }
+        });
+
+        setItems(updatedItems);
+    }
+
+
+    function handleDeleteItem(deletedItem) {
+        const updatedItems = items.filter(item => item.id !== deletedItem.id);
+
+        setItems(updatedItems);
+    } 
+
+
     function handleCategoryChange(category) {
         setSelectedCategory(category);
     }
@@ -39,7 +60,12 @@ function ShoppingList() {
 
             <ul className="Items">
                 {itemsToDisplay.map((item) => (
-                <Item key={item.id} item={item} />
+                <Item 
+                    key={item.id} 
+                    item={item} 
+                    onUpdateItem={handleUpdateItem} 
+                    onDeleteItem={handleDeleteItem}
+                />
                 ))}
             </ul>
         </div>
